806e26c32f4639e78a376bf9396da01c7bda19e6
tunogya <tunogya@qq.com>
2021-10-15 00:01:44 +0800

    simple metadata

--- lib/go/templates/data/cogito_metadata.go
+++ lib/go/templates/data/cogito_metadata.go
@@ -1,51 +1,18 @@
 package data
 
-// IpfsFile is stored on IPFS
-type IpfsFile struct {
-	FileName string `json:",omitempty"`
-	Cid      string `json:",omitempty"`
-}
-
-type Property struct {
-	Attachment       IpfsFile `json:",omitempty"`
-	Copyright        string   `json:",omitempty"`
-	CreatedTimestamp *int32   `json:",omitempty,string"`
-}
-
-type Properties []Property
-
 // CogitoMetadata Cadence requires a mapping of string->string, which can be handled through json tags when marshalling.
 // It also does not allow for null values, so we will be omitting them if empty
 // Reference: https://docs.google.com/spreadsheets/d/1muUZowii0pqoyi6OPK1VPNJi7keSc8_5zI9vu_QvfOY/edit#gid=375111836
 type CogitoMetadata struct {
-	Name        string     `json:",omitempty"`
-	Description string     `json:",omitempty"`
-	Image       IpfsFile   `json:",omitempty"`
-	Properties  Properties `json:",omitempty"`
+	original  string // Original cid of metadata, which may be encrypted
+	temporary string // Storage decoding progress
 }
 
 // GenerateEmptyCogitoMetadata generates a play with all its fields
 // empty except for FullName for testing
-func GenerateEmptyCogitoMetadata(name string) CogitoMetadata {
-	var properties Properties
-	timeStamp := int32(123)
-	property1 := Property{
-		Attachment: IpfsFile{
-			FileName: "attachment1",
-			Cid:      "cid1",
-		},
-		Copyright:        "Test",
-		CreatedTimestamp: &timeStamp,
-	}
-	properties = append(properties, property1)
-
+func GenerateEmptyCogitoMetadata(cid string) CogitoMetadata {
 	return CogitoMetadata{
-		Name:        name,
-		Description: "Description of Cogito",
-		Image: IpfsFile{
-			FileName: "Image Cover",
-			Cid:      "Cid",
-		},
-		Properties: properties,
+		original:  cid,
+		temporary: cid,
 	}
 }
